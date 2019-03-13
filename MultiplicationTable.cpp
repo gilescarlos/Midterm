b5ab436efef76f2d9d0357b0c0d5d7a50a1c8d89
gilescarlos32 <46952491+gilescarlos32@users.noreply.github.com>
2019-03-12 18:38:25 -0700

    Add files via upload

--- MultiplicationTable.cpp
+++ MultiplicationTable.cpp
@@ -8,10 +8,26 @@ int main()
     column = row;
 
     printf("\nMultiplication table from 1 to %d \n\n", row);
-    for(int i = 1; i <= row; i++)
-    {
-        for(int j = 1; j <= column; j++)
-            printf("%5d",i * j);
+
+    printf("\t  ");
+    for(int i = 1; i <= row; i++) {
+        printf("%4d ", i);
+    }
+    printf("\n");
+
+    printf("    +");
+    for(int i = 1; i <= row; i++) {
+        printf("-----");
+    }
+    printf("\n");
+
+
+    for(int i = 1; i <= row; i++) {
+        printf("%d\t|", i);
+
+        for(int j = 1; j <= column; j++) {
+            printf("%5d", i * j);
+        }
         printf("\n");
     }
 
